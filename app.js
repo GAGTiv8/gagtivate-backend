0a1390beeb363eaa7ac0dc51eec11574d772fae1
hendry <hendry_wu1984@yahoo.com>
2020-02-06 18:15:48 +0700

    backend create testing done

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 const express = require('express');
 let app = express();
 const port = 3000;
+const Routes = require('./routes/routes');
+const errHandler = require('./middleware/errHandler.js');
+var cors = require('cors')
 
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
